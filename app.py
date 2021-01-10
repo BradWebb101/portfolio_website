371b03f96157d69a22da4206e45e313da08ede9c
Brad Webb <bradwebb101@outlook.com>
2021-01-10 17:56:13 +0000

    updated  flask app

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+from static.python.get_portfolio_data import get_portfolio_data
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-   return render_template('index.html')
+   portfolio_data = get_portfolio_data()
+   return render_template('index.html', data=portfolio_data)
 
 if __name__ == '__main__':
    app.run(debug=True)
